1d398d68c8e34acef6575cf8ae761fb874e125a4
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-11-26 15:43:44 +1300

    yes not function

--- 01_yes_no_v2.py
+++ 01_yes_no_v2.py
@@ -1,4 +1,3 @@
-
 show_instructions = ""
 while show_instructions.lower() != "xxx":
     # Ask the user if they have played before
@@ -14,6 +13,7 @@ while show_instructions.lower() != "xxx":
         print("program continues")
 
     elif show_instructions == "no" or show_instructions == "n":
+        show_instructions = "no"
         print("display instructions")
 
     else:
