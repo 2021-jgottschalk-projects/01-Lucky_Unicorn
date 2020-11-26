e759c3bcd3d2c5db1b5a16e9fa3ac07f9fbb6b44
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-11-27 10:54:31 +1300

    Developed number checking function

--- 04_how_much_v1.py
+++ 04_how_much_v1.py
@@ -3,19 +3,24 @@
 
 # Main routine goe here
 
-error = "Please enter an whole number between 1 and 10"
+error = "Please enter an whole number between 1 and 10\n"
 
 valid = False
 while not valid:
-    # ask the question
-    response = int(input("How much would you "
-                         "like to play with?"))
-    # if the amount is too low / too high give
-    if 0 < response <= 10:
-        print("You have asked to play with ${}".format(response))
+    try:
+        # ask the question
+        response = int(input("How much would you "
+                             "like to play with? "))
+        # if the amount is too low / too high give
+        if 0 < response <= 10:
+            print("You have asked to play "
+                  "with ${}".format(response))
 
-    # output an error
-    else:
+        # output an error
+        else:
+            print(error)
+
+    except ValueError:
         print(error)
 
-# Print the 
+# Print the
